52f7fb9eee475c303d85e8bfe5b5a22cc9f5999e
Nikita <manager@keenagency.ru>
2016-09-25 22:47:48 +0300

    modular gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,22 @@
+'use strict';
+
+global.$ = {
+    package     : require('./package.json'),
+    config      : require('./gulp/config'),
+    path        : {
+        tasks   : [
+
+        ],
+        //template: require('./gulp/paths/template.js'),
+        //foundation: require('./gulp/paths/foundation.js'),
+        //app: require('./gulp/paths/app.js')
+    },
+    gulp        : require('gulp'),
+    rimraf      : require('rimraf'),
+    browserSync : require('browser-sync').create(),
+    gp          : require('gulp-load-plugins')()
+};
+
 // Define base folders
 var root = './';
 var src  = './app/';
